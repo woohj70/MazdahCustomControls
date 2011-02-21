bdce505ff67726fb3bce66206b082e39aa61804a
mazdah <hyoungjun.woo@gmail.com>
2011-02-22 04:06:42 +0900

    Initial Commit

---
+++ MazdahCustomControls/MazdahCustomControlsAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  MazdahCustomControlsAppDelegate.h
+//  MazdahCustomControls
+//
+//  Created by HYOUNG JUN WOO on 11. 2. 22..
+//  Copyright 2011 Mazdah.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MazdahCustomControlsAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
