966ed4a92e5691079448a707db3ca6b41c90d704
mazdah <hyoungjun.woo@gmail.com>
2011-02-23 01:30:38 +0900

    커스텀 탭바 컨트롤 제작을 위한 ViewController 생

---
+++ MazdahCustomControls/MDCustomTabBarController.h
@@ -0,0 +1,16 @@
+//
+//  MDCustomTabBarController.h
+//  MazdahCustomControls
+//
+//  Created by HYOUNG JUN WOO on 11. 2. 23..
+//  Copyright 2011 Mazdah.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MDCustomTabBarController : UIViewController {
+    
+}
+
+@end
